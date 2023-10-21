cde5461dff00228be7ff605d60d361c675287dd9
Ompio <72317376+Ompio@users.noreply.github.com>
2023-10-21 17:22:04 +0200

    Setting up project

--- eyeTrackingGame/Assets/Camera/CameraContol.cs
+++ eyeTrackingGame/Assets/Camera/CameraContol.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class CameraContol : MonoBehaviour
 {
+    public Transform orientation;
     // Start is called before the first frame update
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
     // Update is called once per frame
